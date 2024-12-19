47eda8824e7b142ace0dbe63c6043f90a24ca263
LuieR <rhluisenrique@gmail.com>
2024-12-19 17:26:46 -0500

    chore: redeployed including new avatar feature - pending edit avatar

--- Api/config/multer.config.js
+++ Api/config/multer.config.js
@@ -11,7 +11,7 @@ const multer = require('multer');
 require('dotenv').config()
 
 cloudinary.config({ 
-  cloud_name: 'dwccysu29', 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
   api_secret: process.env.CLOUDINARY_SECRET , 
 });
